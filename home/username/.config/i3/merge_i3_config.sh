a32287e852d764e75fe6a5a2c5fefd58ee86a673
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2015-12-20 12:42:14 +0100

    some more restructuring

---
+++ home/username/.config/i3/merge_i3_config.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cat base-i3config-common base-i3config-light > config-light
+cat base-i3config-common base-i3config-dark > config-dark
---
+++ home/username/merge_Xresources.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cat base-Xresources-common base-Xresources-light > .Xresources-light
+cat base-Xresources-common base-Xresources-dark > .Xresources-dark
